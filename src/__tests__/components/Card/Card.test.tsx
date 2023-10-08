566a572e9072c8332b331cda5564ad77171db97e
Anthony Silva <thonycout@gmail.com>
2023-10-08 12:51:32 -0300

    card test

---
+++ src/__tests__/components/Card/Card.test.tsx
@@ -0,0 +1,28 @@
+import Card from "@/components/Cards/Card/Card";
+import "@testing-library/jest-dom";
+import { Expense } from "@/models/Expense";
+import { render, screen } from "@testing-library/react";
+
+describe("Card tests", () => {
+    const expense: Expense = {
+        id: 1,
+        name: "Celular",
+        price: 1200,
+        date: new Date(),
+        createdAt: new Date().toLocaleDateString(),
+        user: {
+            id: 1,
+            name: "Anthony",
+            email: "a@a.com",
+        },
+    };
+
+    test("The card should contain the expense infos on the screen", async () => {
+        render(<Card expense={expense} />);
+        const expenseName = await screen.getByText("Celular");
+        const expensePrice = await screen.getByText("1200");
+
+        expect(expenseName).toBeInTheDocument();
+        expect(expensePrice).toHaveTextContent((1200).toString());
+    });
+});
