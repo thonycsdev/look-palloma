92dddd70a30839866b18cc1193b5d634aa4aae3c
Anthony Silva <anthony.eldyn@gmail.com>
2023-10-16 18:30:57 -0300

    react hook form

--- src/pages/expense/[expenseId].tsx
+++ src/pages/expense/[expenseId].tsx
@@ -11,26 +11,35 @@ export default function ExpenseDetails() {
     const expense = getSingleExpense(+expenseId);
     if (!expense) return <h1>Loading...</h1>;
     const formatedDate = expense.date.toLocaleDateString();
+
+    const handleUpdate = () => {
+        console.log("update");
+    };
     return (
         <>
-            <div className="flex flex-col w-full h-auto justify-center items-center gap-7">
-                <div className="flex flex-col">
-                    <label htmlFor="expense-name">Expense:</label>
-                    <input id="expense-name" defaultValue={expense.name} />
+            <form>
+                <div className="flex flex-col w-full h-auto justify-center items-center gap-7">
+                    <div className="flex flex-col">
+                        <label htmlFor="expense-name">Expense:</label>
+                        <input id="expense-name" defaultValue={expense.name} />
+                    </div>
+                    <div className="flex flex-col">
+                        <label htmlFor="expense-price">Price:</label>
+                        <input
+                            id="expense-price"
+                            defaultValue={expense.price}
+                        />
+                    </div>
+                    <div className="flex flex-col">
+                        <label htmlFor="expense-date">Made in:</label>
+                        <input id="expense-date" defaultValue={formatedDate} />
+                    </div>
+                    <div className="flex gap-10">
+                        <Button onClick={handleUpdate}>Update Expense</Button>
+                        <Button>Delete Expense</Button>
+                    </div>
                 </div>
-                <div className="flex flex-col">
-                    <label htmlFor="expense-price">Price:</label>
-                    <input id="expense-price" defaultValue={expense.price} />
-                </div>
-                <div className="flex flex-col">
-                    <label htmlFor="expense-date">Made in:</label>
-                    <input id="expense-date" defaultValue={formatedDate} />
-                </div>
-                <div className="flex gap-10">
-                    <Button>Update Expense</Button>
-                    <Button>Delete Expense</Button>
-                </div>
-            </div>
+            </form>
         </>
     );
 }
